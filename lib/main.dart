7791de3388e12e9b619e37f3157fb4c04dbe1c09
oussamahdidou <oussamahdidou15@gmail.com>
2024-09-28 01:09:27 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/add_diary_screen.dart';
+import 'package:flutter_application_1/screens/home_screen.dart';
+import 'package:flutter_application_1/screens/old_diaries_screen.dart';
+// import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomeScreen(),
+        '/old_diaries': (context) => const OldDiariesScreen(),
+        '/add_diary': (context) => const AddDiaryScreen(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // final int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/add_diary_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
+class AddDiaryScreen extends StatelessWidget {
+  const AddDiaryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('add diary screen')),
+      body: Container(),
+      floatingActionButton: SpeedDial(
+        icon: Icons.menu, // Main button icon when not expanded
+        activeIcon: Icons.close, // Icon when the FAB is expanded
+        backgroundColor: Colors.blue, // Background color of the FAB
+        children: [
+          SpeedDialChild(
+            child: const Icon(Icons.book), // Icon for "See Old Diaries"
+            label: 'See Old Diaries', // Text label for the button
+            backgroundColor: const Color.fromARGB(255, 153, 172, 235),
+            shape:
+                const CircleBorder(), // Background color for this button // Background color for this button
+            onTap: () {
+              Navigator.pushNamed(context, '/old_diaries');
+            },
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.add), // Icon for "Add New Diary"
+            label: 'Add New Diary', // Text label for the button
+            backgroundColor: const Color.fromARGB(255, 153, 172, 235),
+            shape: const CircleBorder(),
+            onTap: () {
+              Navigator.pushNamed(context, '/add_diary');
+            }, // Background color for this button
+          ),
+        ],
+      ),
+    );
+  }
+}
